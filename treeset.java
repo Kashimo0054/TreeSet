949e38c5919483847ea28b0ae2cb08f9c0eb5b02
Sumeet <126050945+Kashimo0054@users.noreply.github.com>
2025-02-23 14:58:58 +0530

    Create treeset.java

---
+++ treeset.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+
+        Set<Integer> nums = new HashSet<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        System.out.println("Set: " + nums);
+    }
+}
